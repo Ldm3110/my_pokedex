861e6241478dd17d2a8b396f76cf52ba17c7711f
DE MATTEIS <ludovic.dematteis@icloud.com>
2023-01-16 22:36:31 +0100

    Create model + make migrations + add app in settings

--- poketeam/models.py
+++ poketeam/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Poketeam(models.Model):
+    name = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False
+    )
+
+    trainer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    )
+
+    class Meta:
+        verbose_name = "Poketeam"
+        verbose_name_plural = "Poketeams"
+
+    def __str__(self) -> str:
+        return f"{self.name} trained by {self.trainer}"
