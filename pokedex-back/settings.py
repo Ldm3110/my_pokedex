c8ee679b3764297813364cd798557fa4796c8f17
DE MATTEIS <ludovic.dematteis@icloud.com>
2023-01-23 16:02:36 +0100

    update settings.py

--- pokedex-back/settings.py
+++ pokedex-back/settings.py
@@ -89,7 +89,6 @@ WSGI_APPLICATION = "pokedex-back.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-print(f"\nargv = {sys.argv}\n")
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("DB_ENGINE"),
@@ -102,8 +101,12 @@ DATABASES = {
 }
 # Used a sqlite DB for tests and tests with circleCI
 if 'pytest' in sys.argv[0]:
-    print("\nsqlite\n")
-    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+    DATABASES = {
+        "default": {
+            "ENGINE": 'django.db.backends.sqlite3',
+            "NAME": ":memory:",
+        }
+    }
 
 
 # Password validation
